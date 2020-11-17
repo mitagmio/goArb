b4dba55efbdcb86d0e5d04f3f4e0e906c6063321
Mitag Mio <mitag.mio@gmail.com>
2020-11-17 09:20:53 +0100

    start

---
+++ main.go
@@ -0,0 +1,16 @@
+/*
+	Бот создан в рамках проекта EBLAVOD, подробнее в телеграмм
+	@ncplatzdarm @mitagmio
+	или на форуме https://forum.mhub.to/
+*/
+
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+)
